b04688c47d98e2e26694e5f53b11667ebd717380
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-03-28 03:56:44 +0600

    Implement subclassing for custom errors and convert errors to responses

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,14 @@
+export class DatabaseConnectionError extends Error {
+    statusCode = 500;
+    reason = 'Error connecting to Database';
+
+    constructor() {
+        super();
+
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeError() {
+        return [{ message: this.message }];
+    }
+}
