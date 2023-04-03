eab7ee6d590498ee76cd5d471cc95d93acc20f29
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-04-03 16:50:37 +0600

    Add middleware to validate request in signup and signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin',
+router.post(
+    '/api/users/signin',
     [
         body('email')
             .isEmail()
@@ -15,12 +16,9 @@ router.post('/api/users/signin',
             .notEmpty()
             .withMessage('You must supply a password')
     ],
+    validateRequest,
     (req: Request, res: Response) => {
-        const errors = validationResult(req);
 
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
     }
 );
 
