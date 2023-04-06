ea1b760b5f79b85655f9aa518d228695f9fde782
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-04-06 23:55:37 +0600

    Create signup.test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -12,6 +12,8 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+    process.env.JWT_KEY = 'asdf';
+
     const collections = await mongoose.connection.db.collections();
 
     for (let collection of collections) {
