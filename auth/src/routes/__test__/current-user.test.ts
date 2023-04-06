823bf577c6965c48a0defb04c20a242935577188
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-04-07 03:23:39 +0600

    Add auth-helper function to get cookie

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,14 +2,8 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about the current user', async () => {
-    const authResponse = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201);
-    const cookie = authResponse.get('Set-Cookie');
+
+    const cookie = await global.signin();
 
     const response = await request(app)
         .get('/api/users/currentuser')
@@ -17,5 +11,5 @@ it('responds with details about the current user', async () => {
         .send()
         .expect(200);
 
-    expect(response.body.currentUser.email).toBe('test@test.com');
+    expect(response.body.currentUser.email).toEqual('test@test.com');
 });
