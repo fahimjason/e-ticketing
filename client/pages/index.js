b09468ca8ae72cdbe58a00c157365746151bdb49
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-04-10 03:54:56 +0600

    Spacify the host and pass cookie in servier side rendering

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,33 @@
-const Home = () => {
-    return <h1> Home </h1>;
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
+    // axios.get('/api/users/currentuser').catch((err) => {
+    //     console.log(err.message);
+    // });
+
+    return <h1>Landing Page</h1>;
 };
 
-export default Home;
+LandingPage.getInitialProps = async ({ req }) => {
+    if (typeof window === "undefined") {
+        // we are on server! 
+        // requests should be made to http://SERVICENAME..NAMESPACE.svc.cluster.local/...
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+            headers: req.headers
+        });
+
+        return data;
+
+    } else {
+        // we are on browser!
+        // requests can be made with a base url of  ``
+        const { data } = await axios.get('/api/users/currentuser');
+
+        return data;
+
+    }
+};
+
+export default LandingPage;
