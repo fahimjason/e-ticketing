b3988d21797e0dd5fe07a7ec073c7135a8eede1f
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-04-19 16:57:37 +0600

    Test invalid input and check ticket updating

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -47,7 +47,63 @@ it('returns a 401 if the user does not own the ticket', async () => {
         .expect(401);
 });
 
-it('returns a 400 if the user provides an invalid title or price', async () => { });
+it('returns a 400 if the user provides an invalid title or price', async () => {
+    const cookie = global.signin();
 
-it('update ticket provided valid inputs', async () => { });
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'Title',
+            price: 20
+        })
+        .expect(201);
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: '',
+            price: 299
+        })
+        .expect(400);
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: '',
+            price: -20
+        })
+        .expect(400);
+});
+
+it('update ticket provided valid inputs', async () => {
+    const cookie = global.signin();
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'Title',
+            price: 20
+        })
+        .expect(201);
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'New title',
+            price: 299
+        })
+        .expect(200);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+
+    expect(ticketResponse.body.title).toEqual('New title');
+    expect(ticketResponse.body.price).toEqual(299);
+});
 
