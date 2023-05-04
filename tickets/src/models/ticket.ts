3869e7b6b6af9e25e67ad552768dd7bca4ef1021
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-05-04 10:41:03 +0600

    Implement optimistic concurrency control with mongoose in the tickets-service with unit test

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttars {
     title: string;
@@ -10,6 +11,7 @@ interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
     userId: string;
+    version: number;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -37,6 +39,9 @@ const ticketSchema = new mongoose.Schema({
         }
     }
 });
+
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttars) => {
     return new Ticket(attrs);
