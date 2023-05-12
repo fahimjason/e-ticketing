bbbd0ed0df64698df6a96ff937a4c6a5e566b2bc
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-05-12 20:12:19 +0600

    Implement order-created-listener, expiration-complete-publisher and expiration-queue in expiration-service

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,26 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@e-ticketing/common';
+import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        const delay = new Date(data.expireAt).getTime() - new Date().getTime();
+        console.log('Waiting this many milliseconds to process the job:', delay);
+
+        await expirationQueue.add(
+            {
+                orderId: data.id
+            },
+            {
+                delay: 10000
+            }
+        );
+
+        msg.ack();
+    }
+
+}
