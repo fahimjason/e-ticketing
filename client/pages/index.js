5b2ea4a0ff562005c45a69823a6195600708e71d
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-05-24 21:23:33 +0600

    Implement ticket creation and ticket list in client-service

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,34 @@
-const LandingPage = ({ currentUser }) => {
+const LandingPage = ({ currentUser, tickets }) => {
+    const ticketList = tickets.map(ticket => {
+        return (
+            <tr key={ticket.id}>
+                <td>{ticket.title}</td>
+                <td>{ticket.price}</td>
+            </tr>
+        );
+    })
 
-    return (currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>);
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ticketList}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
+    const { data } = await client.get('/api/tickets');
 
-    return {};
+    return { tickets: data };
 };
 
 export default LandingPage;
