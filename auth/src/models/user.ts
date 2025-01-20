dd36adaba17706694b8f87a5ddc6b1a8b468ee17
Fahimul Hoque Jason <63965475+fahimjason@users.noreply.github.com>
2025-01-20 12:25:24 +0600

    Update user.ts to fix the auth model issue

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -44,8 +44,8 @@ const userSchema = new mongoose.Schema({
 );
 
 userSchema.pre('save', async function (done) {
-    if (this.isModified('password')) {
-        const hashed = await Password.toHash(this.get('password'));
+    if (this.isModified('password')!) {
+        const hashed = await Password.toHash(this.get('password')!);
         this.set('password', hashed);
     }
     done();
